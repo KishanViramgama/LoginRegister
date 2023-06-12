ffc12b3edc6f5a793c0a0736dbd32a5e7d1666d3
KishanViramgama <viramgama.kishan@gmail.com>
2023-06-12 22:24:33 +0530

    Improvement

--- app/src/main/java/com/app/loginregister/util/BasePath.kt
+++ app/src/main/java/com/app/loginregister/util/BasePath.kt
@@ -2,7 +2,7 @@ package com.app.loginregister.util
 
 object BasePath {
 
-    const val baseUrl = "http://3.110.41.106/"
+    const val baseUrl = ""
     private const val baseFolderPath = "trainee/api/"
 
     const val login = "${baseFolderPath}login.php"
